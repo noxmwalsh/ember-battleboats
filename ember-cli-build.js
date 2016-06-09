c86e7bddb19cb26702b7243eac2fa246e8f4ed8e
noxmwalsh <noxiousthing@gmail.com>
2016-06-08 19:50:02 -0700

    Import bootstrap related stuff into the Broccoli build process

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,10 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    'ember-cli-bootstrap': {
+      'importBootstrapJS': true,
+      'importBootstrapTheme': true
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
