09f4f052f12e96185ebf2b1a1240a598b27e4920
noxmwalsh <noxiousthing@gmail.com>
2016-06-08 20:53:30 -0700

    Start rendering the board

--- app/components/game-board-grid/component.js
+++ app/components/game-board-grid/component.js
@@ -3,7 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   //10x10 from 1-10 to A-J
 
-  cells: null,
+  gameBoard: null,
+  cells: Ember.computed.alias('gameBoard.cells'),
+
+  cellRows: Ember.computed('cells.[]', function() {
+    let rows = Ember.A();
+    for(let i = 1; i <= 10; i++) {
+      let cells = this.get('cells').filterBy('row', i);
+      rows.push(Ember.Object.create('cells', cells));
+    }
+    return rows;
+  }),
+
 
   action: {
     fireRound: function() {
